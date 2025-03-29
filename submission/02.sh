a0e8553dd3a4e1d76fdbf58c151edaf17e988a3e
codaMW <codamw011@gmail.com>
2025-03-29 14:00:37 +0000

    02.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,7 @@
 # Create a native segwit address and get the public key from the address.
+
+native_segwit=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+
+pubKey=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $native_segwit | jq -r '.pubkey')
+
+echo "$pubKey"
